05d0a300fa1f93985bb00b08b8c426d35b79c3ac
DoLong96 <manhlong1361996@gmail.com>
2019-05-15 15:16:16 +0700

    test heroku

---
+++ src/main/java/com/example/login/config/DataSeedingListener.java
@@ -0,0 +1,63 @@
+package com.example.login.config;
+
+import java.util.HashSet;
+
+import com.example.login.Entity.Role;
+import com.example.login.Entity.User;
+import com.example.login.Repository.RoleRepository;
+import com.example.login.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class DataSeedingListener implements ApplicationListener<ContextRefreshedEvent> {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent arg0) {
+        // Roles
+        if (roleRepository.findByName("ROLE_ADMIN") == null) {
+            roleRepository.save(new Role("ROLE_ADMIN"));
+        }
+
+        if (roleRepository.findByName("ROLE_USER") == null) {
+            roleRepository.save(new Role("ROLE_USER"));
+        }
+
+        // Admin account
+        if (userRepository.findByEmail("admin@gmail.com") == null) {
+            User admin = new User();
+            admin.setEmail("admin@gmail.com");
+            admin.setPassword(passwordEncoder.encode("123456"));
+            HashSet<Role> roles = new HashSet<>();
+            roles.add(roleRepository.findByName("ROLE_ADMIN"));
+            roles.add(roleRepository.findByName("ROLE_USER"));
+            admin.setRoles(roles);
+            userRepository.save(admin);
+        }
+
+        // Member account
+        if (userRepository.findByEmail("member@gmail.com") == null) {
+            User user = new User();
+            user.setEmail("member@gmail.com");
+            user.setPassword(passwordEncoder.encode("123456"));
+            HashSet<Role> roles = new HashSet<>();
+            roles.add(roleRepository.findByName("ROLE_USER"));
+            user.setRoles(roles);
+            userRepository.save(user);
+        }
+    }
+
+}
